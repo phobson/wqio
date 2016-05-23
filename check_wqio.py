2310466c8339de21213ba08545e4752724794986
Paul Hobson <pmhobson@gmail.com>
2016-05-22 20:40:42 -0700

    first rough doc build

--- check_wqio.py
+++
@@ -1,7 +0,0 @@
-import sys
-import matplotlib
-matplotlib.use('agg')
-
-import wqio
-status = wqio.test(*sys.argv[1:])
-sys.exit(status)
--- wqio/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .misc import *
-from .dateutils import *
-from .numutils import *
-from .reportutils import *
-from . import figutils
